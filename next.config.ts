2859219a99ec3185e38e4d0e4265ed6688eb040d
Stephen F <snipeydev@gmail.com>
2025-02-23 01:46:58 -0800

    pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
-if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
-}
-export const runtime = "edge";
+
 export default nextConfig;
